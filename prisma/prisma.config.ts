97a8112784d3be2770ec9f3c9d18bda9caff01ff
Aquila-M <atemoraiscosta88@gmail.com>
2025-12-12 19:57:31 -0300

    fix: prisma.config

--- prisma/prisma.config.ts
+++ prisma/prisma.config.ts
@@ -1,12 +1,20 @@
 import 'dotenv/config'
-import { defineConfig, env } from 'prisma/config'
+import { defineConfig } from 'prisma/config'
+import { Pool } from 'pg'
+import path from 'path'
 
 export default defineConfig({
-  schema: 'prisma/schema.prisma',
+  earlyAccess: true,
+  schema: path.join(__dirname, 'schema.prisma'),
   migrations: {
-    path: 'prisma/migrations',
+    path: path.join(__dirname, 'migrations'),
   },
   datasource: {
-    url: env('DIRECT_URL'),
+    url: process.env.DIRECT_URL!
   },
+  migrate: {
+    adapter: async () => {
+      return new Pool({ connectionString: process.env.DIRECT_URL })
+    }
+  }
 })
